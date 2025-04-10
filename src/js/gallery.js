6a2b8a58387fc3d6076a47d946b7b0575e372342
Anastasiia Skliarovska <skliarovskaa@gmail.com>
2025-04-10 21:57:31 +0300

    add gallere section

---
+++ src/js/gallery.js
@@ -0,0 +1,31 @@
+import Swiper from 'swiper';
+import 'swiper/css';
+import 'swiper/css/effect-coverflow';
+
+const swiper = new Swiper('.swiper', {
+  effect: 'coverflow',
+  grabCursor: true,
+  centeredSlides: true,
+  initialSlide: 2,
+  speed: 600,
+  preventClicks: true,
+  slidesPerView: 3,
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 0,
+    depth: 100,
+    modifier: 4,
+    slideShadows: true,
+  },
+  loop: true,
+  navigation: {
+    nextEl: '.gallery-btn-next',
+    prevEl: '.gallery-btn-prev',
+  },
+  keyboard: {
+    enabled: true,
+  },
+  mousewheel: {
+    thresholdDelta: 70,
+  },
+});
