65b7186b8079a822d7aac1a7995688e5966bce82
Anastasiia Skliarovska <skliarovskaa@gmail.com>
2025-04-11 14:58:03 +0300

    add gallery swiper

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -1,31 +1,57 @@
 import Swiper from 'swiper';
-import 'swiper/css';
-import 'swiper/css/effect-coverflow';
+import { Navigation } from 'swiper/modules';
 
-const swiper = new Swiper('.swiper', {
-  effect: 'coverflow',
-  grabCursor: true,
+const swiper = new Swiper('.gallery-swiper', {
   centeredSlides: true,
-  initialSlide: 2,
   speed: 600,
-  preventClicks: true,
-  slidesPerView: 3,
-  coverflowEffect: {
-    rotate: 0,
-    stretch: 0,
-    depth: 100,
-    modifier: 4,
-    slideShadows: true,
+  breakpoints: {
+    320: {
+      slidesOffsetBefore: 70,
+      spaceBetween: 12,
+      slidesPerView: 'auto',
+    },
+    1200: {
+      slidesOffsetBefore: 24,
+      spaceBetween: 24,
+      slidesPerView: 3,
+    },
   },
-
-  navigation: {
-    nextEl: '.gallery-btn-next',
-    prevEl: '.gallery-btn-prev',
-  },
+  initialSlide: 1,
+  touchEventsTarget: 'swiper-wrapper',
   keyboard: {
-    enabled: true,
+    enabled: false,
+    onlyInViewport: false,
   },
   mousewheel: {
-    thresholdDelta: 70,
+    invert: false,
+  },
+  on: {
+    slideChange() {
+      updateActiveSlide(this);
+    },
+    init() {
+      updateActiveSlide(this);
+      updateteButtons(this);
+    },
   },
 });
+
+function updateActiveSlide(swiper) {
+  document.querySelectorAll('.swiper-slide').forEach((slide, index) => {
+    slide.classList.toggle('active', index === swiper.realIndex);
+  });
+}
+
+function updateteButtons(swiper) {
+  const btnNext = document.querySelector('.swiper-btn-next');
+  const btnPrev = document.querySelector('.swiper-btn-prev');
+
+  btnNext.addEventListener('click', () => {
+    console.log('next');
+    swiper.slideNext();
+  });
+  btnPrev.addEventListener('click', () => {
+    console.log('prev');
+    swiper.slidePrev();
+  });
+}
